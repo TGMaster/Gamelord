265e740718a8208a782a173eba8ec965ca5c6e14
TG <TGMaster@users.noreply.github.com>
2017-12-14 22:45:54 +0700

    Add AUTO_INCREMENT

--- create_database.sql
+++ create_database.sql
@@ -3,13 +3,13 @@
 -- ----------------------------
 
 CREATE TABLE IF NOT EXISTS `news` (
-  `news_id` bigint(11) NOT NULL,
+  `news_id` bigint(11) NOT NULL AUTO_INCREMENT,
   `news_title` varchar(128) NOT NULL,
   `news_content` longtext NOT NULL,
-  `news_date` varchar(64) NOT NULL,
+  `news_date` date NOT NULL,
   `news_image` varchar(64) NOT NULL,
   PRIMARY KEY (`news_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
 
 -- --------------------------------------------------------
 
@@ -17,10 +17,10 @@ CREATE TABLE IF NOT EXISTS `news` (
 -- Table structure for category
 -- ----------------------------
 CREATE TABLE IF NOT EXISTS `category` (
-  `category_id` bigint(11) NOT NULL,
+  `category_id` bigint(11) NOT NULL AUTO_INCREMENT,
   `category_name` varchar(128) NOT NULL,
   PRIMARY KEY  (`category_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
 
 -- --------------------------------------------------------
 
@@ -29,7 +29,7 @@ CREATE TABLE IF NOT EXISTS `category` (
 -- ----------------------------
 
 CREATE TABLE IF NOT EXISTS `product` (
-  `product_id` bigint(20) NOT NULL,
+  `product_id` bigint(20) NOT NULL AUTO_INCREMENT,
   `category_id` bigint(11) default NULL,
   `product_name` varchar(255) collate utf8_unicode_ci default NULL,
   `product_image` varchar(64) collate utf8_unicode_ci default NULL,
@@ -44,7 +44,7 @@ CREATE TABLE IF NOT EXISTS `product` (
 -- Table structure for users
 -- ----------------------------
 CREATE TABLE IF NOT EXISTS `users` (
-  `user_id` bigint(20) NOT NULL,
+  `user_id` bigint(20) NOT NULL AUTO_INCREMENT,
   `user_email` varchar(255) collate utf8_unicode_ci default NULL,
   `user_pass` varchar(255) collate utf8_unicode_ci default NULL,
   `user_role` bit(1) default NULL,
